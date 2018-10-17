faa1bf858d56511016f67d6fc8dca08700585187
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-16 22:26:30 -0400

    Use Incrementor components to adjust state in App

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,55 @@ import Timer from './Timer.js';
 import Incrementor from './Incrementor.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBreakTime: false,
+      workTimeLength: 25,
+      breakTimeLength: 5
+    };
+
+  }
+
+  incrementLength(lengthName) {
+    let newState = {};
+    newState[lengthName] = this.state[lengthName] + 1;
+    this.setState(newState);
+  }
+
+  decrementLength(lengthName) {
+    let newState = {};
+
+    //do not let length drop below zero
+    if (this.state[lengthName] > 0) {
+        newState[lengthName] = this.state[lengthName] - 1;
+    }
+
+    this.setState(newState);
+  }
+
   render() {
     return (
       <div>
           <h1>Pomodoro Timer</h1>
-          <h3>Mode</h3>
+          <h3>{this.state.isBreakTime ? "Time to relax!" : "Time to work!"}</h3>
           <Timer />
-          <span>Work Length: <Incrementor /></span>
-          <span>Break Length: <Incrementor /></span>
+          <span>
+            Work Length:
+           <Incrementor
+            value={this.state.workTimeLength}
+            onIncrement={this.incrementLength.bind(this, 'workTimeLength')}
+            onDecrement={this.decrementLength.bind(this, 'workTimeLength')}
+            />
+          </span>
+          <span>
+            Break Length:
+            <Incrementor
+              value={this.state.breakTimeLength}
+              onIncrement={this.incrementLength.bind(this, 'breakTimeLength')}
+              onDecrement={this.decrementLength.bind(this, 'breakTimeLength')}
+              />
+          </span>
       </div>
     );
   }
